4a7a832ab66452bea0f90acc90612cb87e238e65
Sam Billingham <sambillie.sb@gmail.com>
2018-12-06 16:44:16 +0100

    add first component - SteemFeed

---
+++ src/components/SteemFeed.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="steemfeed">
+        <h1>Steem Feed</h1>
+    </div>
+</template>
+    
+<script>
+    export default {
+        name: 'SteemFeed'
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SteemFeed/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SteemFeed from '@/components/SteemFeed.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    SteemFeed
   }
 }
 </script>
