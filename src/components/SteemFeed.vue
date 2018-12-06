5fcf8172931c68c5b452c3414bae90ad720e8cc7
Sam Billingham <sambillie.sb@gmail.com>
2018-12-06 16:48:39 +0100

    store steem post results as component data and loop in template

--- src/components/SteemFeed.vue
+++ src/components/SteemFeed.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="steemfeed">
         <h1>Steem Feed</h1>
+        <main v-for="post in posts" :key="post.id">
+            <h2>{{post.title}}</h2>
+         </main>
     </div>
 </template>
     
@@ -8,11 +11,16 @@
     const dsteem = require('dsteem')
     export default {
         name: 'SteemFeed',
+        data () {
+            return {
+                posts: null
+            }
+        },
         created () {
             const client = new dsteem.Client('https://api.steemit.com')
             client.database.getDiscussions('blog', { tag: 'sambillingham', limit: 20 })
             .then(result => {
-                console.log(result)
+                this.posts = result
             })
         }
     }
