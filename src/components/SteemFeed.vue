6807838e19c3ecb1a8ff81c21fde2bb49069a721
Sam Billingham <sambillie.sb@gmail.com>
2018-12-06 16:46:07 +0100

    add dsteem and request user blog posts

--- src/components/SteemFeed.vue
+++ src/components/SteemFeed.vue
@@ -5,7 +5,15 @@
 </template>
     
 <script>
+    const dsteem = require('dsteem')
     export default {
-        name: 'SteemFeed'
+        name: 'SteemFeed',
+        created () {
+            const client = new dsteem.Client('https://api.steemit.com')
+            client.database.getDiscussions('blog', { tag: 'sambillingham', limit: 20 })
+            .then(result => {
+                console.log(result)
+            })
+        }
     }
 </script>
