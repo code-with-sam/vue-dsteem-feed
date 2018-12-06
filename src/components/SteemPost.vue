41cc194a34c60f31ef3883d53266b28c00f95374
Sam Billingham <sambillie.sb@gmail.com>
2018-12-06 16:55:42 +0100

    stoe steem post response data and display raw markdown in template

--- src/components/SteemPost.vue
+++ src/components/SteemPost.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>STEEM Post</h1> 
+    <h1>{{ title }}</h1>
+    <main>{{ content }}</main>
   </div>
 </template>
 
@@ -11,13 +12,16 @@ export default {
   name: 'SteemPost',
   data () {
     return {
+        title: null,
+        content: null
     }
   },
   created () {
     const client = new dsteem.Client('https://api.steemit.com')
     client.call('condenser_api', 'get_content', [this.$route.params.username, this.$route.params.permlink])
     .then(response => {
-        console.log(response)
+        this.title = response.title
+        this.content = response.body
     })
   }
 }
